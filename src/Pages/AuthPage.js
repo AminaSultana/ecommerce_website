46159990da64c79333b732cc825ac623c412f71a
AminaSultana <lilisultana53@gmail.com>
2022-05-08 00:49:19 +0530

    task 22

---
+++ src/Pages/AuthPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from '../Component/Auth/Login';
+
+const AuthPage = () => {
+  return (
+    <div>
+      <Login/>
+    </div>
+  );
+}
+
+export default AuthPage;
--- src/Store/CartProvider.js
+++ src/Store/CartProvider.js
@@ -1,14 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useContext,useCallback, useEffect } from "react";
 
 import CartContext from "./cart_context";
+import AuthContext from "./auth-context";
 
 const CartProvider = (props) => {
+  const authCtx = useContext(AuthContext);
   const [cartItems, setCartItems] = useState([]);
   const [totalAmount, setTotalAmount] = useState(0);
   const [totalQuantity, setTotalQuantity] = useState(0);
 
-  const addItemToCartHandler = (item) => {
-    const updatedTotalAmount = cart_context.totalAmount + item.price;
+  let newEmailString, email;
+  if(authCtx.isLoggedIn){
+    newEmailString = authCtx.idToken.toString();
+    email = newEmailString.replace(/[@.]/gi, '');
+  }
+  console.log(email);
+
+  const getCartItemFromDb = useCallback(async () => {
+    console.log(" getcartitem called");
+    const response = await fetch(
+      `https://crudcrud.com/api/b59c4d70a3b747e7a429fb44385f89c9/cart${email}`
+    );
+    const data = await response.json();
+    const cartItem = [];
+    let amount = 0;
+    for (const key in data) {
+      amount += data[key].price;
+      cartItem.push({
+        id: data[key].id,
+        title: data[key].title,
+        price: data[key].price,
+      });
+    }
+    setTotalAmount(amount)
+    setCartItems(cartItem);
+  }, [email]);
+
+  useEffect(() => {
+    getCartItemFromDb()
+  }, [getCartItemFromDb]);
+
+  
+  const addItemToCartHandler = async (item) => {
+    const response = await fetch(
+      `https://crudcrud.com/api/b59c4d70a3b747e7a429fb44385f89c9/cart${email}`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          userid: email,
+          id: item.id,
+          price: item.price,
+          title: item.title,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (response.ok) {
+      setTotalQuantity(prev=>prev+1)
+    }
+
+    /*  const updatedTotalAmount = cart_context.totalAmount + item.price;
     setTotalAmount(updatedTotalAmount);
     setTotalQuantity((preQty) => preQty + 1);
     const itemIdx = cartItems.findIndex((i) => i.id === item.id);
@@ -24,7 +77,7 @@ const CartProvider = (props) => {
       return;
     }
     let updatedItem = { ...item, quantity: 1 };
-    setCartItems([...cartItems, updatedItem]);
+    setCartItems([...cartItems, updatedItem]); */
   };
 
   const removeItemToCartHandler = (id) => {};
